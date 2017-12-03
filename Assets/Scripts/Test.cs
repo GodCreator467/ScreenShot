7383e6bd95ba852811661f19c4cb91156de9f2a5
GodCreator <theenigma@yandex.ru>
2017-12-04 02:54:20 +0300

    Init

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+using System.IO;
+
+/*
+ * https://github.com/ChrisMaire/unity-native-sharing
+ */
+
+public class Test : MonoBehaviour {
+	public string ScreenshotName = "screenshot.png";
+
+    public void ShareScreenshotWithText(string text)
+    {
+        string screenShotPath = Application.persistentDataPath + "/" + ScreenshotName;
+        if(File.Exists(screenShotPath)) File.Delete(screenShotPath);
+
+        Application.CaptureScreenshot(ScreenshotName);
+
+        StartCoroutine(delayedShare(screenShotPath, text));
+    }
+
+    IEnumerator delayedShare(string screenShotPath, string text)
+    {
+        while(!File.Exists(screenShotPath)) {
+    	    yield return new WaitForSeconds(.05f);
+        }
+
+		NativeShare.Share(text, screenShotPath, "", "", "image/png", true, "");
+        print(Application.persistentDataPath);//вывод изображения
+    }
+}
